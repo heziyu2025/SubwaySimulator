347ab6f3e1a33fe965e358d7a508198d246804e3
heziyu <ziyu2025@gmail.com>
2023-06-10 21:46:03 +0800

    added car control.

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class Control : MonoBehaviour
 {
+    public float acc = 0.5f;
+    public float speed = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        //Logger('Hello World');
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 a = gameObject.transform.localPosition;
+        a.x += speed;
+        gameObject.transform.localPosition = a;
+
         if (Input.GetKey(KeyCode.W))
         {
-            Vector3 a = gameObject.transform.position;
-            a.x += 0.1f;
-            gameObject.transform.position = a;
+            speed += acc / 1000;
+        } else
+        {
+            speed *= 0.99f;
         }
     }
 }
