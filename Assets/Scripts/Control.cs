36799b689f17c1283a1e3047adbf264e66ff5c9c
heziyu <ziyu2025@gmail.com>
2023-06-10 20:21:35 +0800

    object control

--- Assets/Scripts/Control.cs
+++ Assets/Scripts/Control.cs
@@ -7,12 +7,17 @@ public class Control : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Logger('Hello World');
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            Vector3 a = gameObject.transform.position;
+            a.x += 0.1f;
+            gameObject.transform.position = a;
+        }
     }
 }
